60462bba8e15f601313ae50d72e6e72166ef0af7
Zeninit <zenin3958@gmail.com>
2024-03-01 18:31:33 +0700

    Add mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,55 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutLinedIcon from '@mui/icons-material/DarkModeOutLined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const seclectedMode = event.target.value
+    setMode(seclectedMode)
+  }
+
+  return (
+    <FormControl
+      sx={{ m: 1, minWidth: 120 }}
+      size='small'>
+      <InputLabel id='label-seclect-dark-light-mode'>Mode</InputLabel>
+      <Select
+        labelId='label-seclect-dark-light-mode'
+        id='seclect-dark-light-mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}>
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutLinedIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -21,6 +70,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <div>Zenin-dev</div>
 
