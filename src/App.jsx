283c4fbda3417c437ed09692b8f584686435901e
Zeninit <zenin3958@gmail.com>
2024-03-01 15:52:30 +0700

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+
 function App() {
   return (
     <>
       <div>Zenin-dev</div>
+
+      <Typography
+        variant='body2'
+        color='text.secondary'>
+        Test Typography
+      </Typography>
+
       <Button variant='contained'>Hello world</Button>
       <Button variant='text'>Text</Button>
       <Button variant='contained'>Contained</Button>
