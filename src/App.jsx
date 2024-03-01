1d76b65f0a52f76cb8538a32a7075196fd42f57e
Zeninit <zenin3958@gmail.com>
2024-03-01 17:04:18 +0700

    Edit light dark base

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}>
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>Zenin-dev</div>
 
       <Typography
