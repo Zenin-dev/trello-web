c95a9b4994a6539c4fc7d4dee8e9345522751bd5
Zeninit <zenin3958@gmail.com>
2024-03-11 20:37:44 +0700

    Fix Build Mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-smva.onrender.com'
+let apiRoot = ''
+
+if (import.meta.env.DEV) {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (import.meta.env.PROD) {
+  apiRoot = 'https://trello-api-smva.onrender.com'
+}
+
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.evn': {}
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
